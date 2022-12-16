edd0a57e74c43d6afd8ff3138983c414eb1a462b
scx1332 <sieciech.czajka@golem.network>
2022-12-16 18:56:09 +0100

    Removed unused code in main

--- src/main.rs
+++ src/main.rs
@@ -16,47 +16,6 @@ use options::validated_cli;
 
 pub mod options;
 
-struct _Web3ChainConfig {
-    glm_token: Address,
-    chain_id: u64,
-    erc20_contract: Contract<Http>,
-}
-
-struct HexSlice<'a>(&'a [u8]);
-
-impl<'a> HexSlice<'a> {
-    fn new<T>(data: &'a T) -> HexSlice<'a>
-    where
-        T: ?Sized + AsRef<[u8]> + 'a,
-    {
-        HexSlice(data.as_ref())
-    }
-}
-
-// You can choose to implement multiple traits, like Lower and UpperHex
-impl fmt::Display for HexSlice<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for byte in self.0 {
-            // Decide if you want to pad the value or have spaces inbetween, etc.
-            write!(f, "{:X} ", byte)?;
-        }
-        Ok(())
-    }
-}
-
-trait HexDisplayExt {
-    fn hex_display(&self) -> HexSlice<'_>;
-}
-
-impl<T> HexDisplayExt for T
-where
-    T: ?Sized + AsRef<[u8]>,
-{
-    fn hex_display(&self) -> HexSlice<'_> {
-        HexSlice::new(self)
-    }
-}
-
 async fn main_internal() -> Result<(), PaymentError> {
     if let Err(err) = dotenv::dotenv() {
         return Err(err_custom_create!("No .env file found: {}", err));
